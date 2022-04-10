40a5dd732b248c9dc841630d162221a86487ebfc
Igor Kravtsov <kravtsov.igor.karate@gmail.com>
2022-04-11 02:07:21 +0300

    changed main color

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -2,18 +2,8 @@ import { createTheme } from '@mui/material/styles'
 
 export const theme = createTheme({
   palette: {
-    // mode: 'light',
     primary: {
-      main: '#8d00de',
-      // light: 'rgb(163, 51, 228)',
-      // dark: 'rgb(98, 0, 155)',
-      // contrastText: '#fff',
+      main: '#6d24f1',
     },
-    // secondary: {
-    //   main: '#005dd4',
-    // },
-    // error: {
-    //   main: '#de3023',
-    // },
   },
 })
